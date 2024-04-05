9bfde9bf3a708ff3f1f427e96a8f8df970db5ba8
GreenMask2003 <darjikushagra@gmail.com>
2024-04-05 15:10:12 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";                         // for port values defining 
 import cookieParser from "cookie-parser";            //for calling middleware
@@ -12,6 +13,8 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000 ;              // If you wantwed to use any-other port
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());                             // to parse the requests with JSON payloads( authcontroller.js = req.body modules)
@@ -20,6 +23,12 @@ app.use(cookieParser());                             // to access cookies before
 app.use("/api/auth", authRoutes)                     // used for not writing login/signup and all in and out routes
 app.use("/api/messages", messageRoutes) 
 app.use("/api/users", userRoutes) 
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // app.get('/', (req, res) => {
 //     res.send("Hello, world!!!!!");                   // localhost= 5000 is the home/host page for "/" reference mentioned
